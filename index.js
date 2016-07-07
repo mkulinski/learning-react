a9bbc5e8731de2ebb549409a7bc1884ebe97388c
mkulinski <me@michaelkulinski.com>
2016-07-06 18:22:48 -0700

    create a mirroring text app

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloUser = React.createClass({
-    getInitialState: function() {
-        return {
-            username: 'AnnonymousUser'
-        }
+var ChangeName = React.createClass({
+    getInitialState: function(){
+      return {
+          username: '@michael'
+      }
     },
-    handleChange: function(e) {
-      this.setState({
-          username: e.target.value
-      });
+    changeTheText: function(e){
+        this.setState({
+            username: e.target.value
+        });
     },
-    render: function() {
+    render: function(){
         return (
             <div>
-                <h1>Hello {this.state.username}</h1><br />
-                Change name: <input type="text" value={this.state.username} onChange={this.handleChange} />
+                <h1>Hello {this.state.username}!!!!!</h1>
+                <input type="text" value={this.state.username} onChange={this.changeTheText} />
             </div>
-        )
+        );
     }
 });
 
-ReactDOM.render(<HelloUser />, document.getElementById('root'));
+ReactDOM.render(<ChangeName />, document.getElementById('app'));
